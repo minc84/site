78a9638c9e83536b6b42a2a30fc8aa481bdb726d
Nikolay Nesteroich <nesterovish@yandex.ru>
2020-01-29 23:07:42 +0300

    Добавлена почта

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import Form
-from wtforms import TextField, TextAreaField, SubmitField, validators
+from flask_wtf import FlaskForm
+from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError
 
  
-class ContactForm(Form):
-  name = TextField("Name")
-  email = TextField("Email")
-  subject = TextField("Subject")
-  message = TextAreaField("Message")
+class ContactForm(FlaskForm):
+  name = TextField("Name",  [validators.Required("Please enter your name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
+  message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
   submit = SubmitField("Send")
