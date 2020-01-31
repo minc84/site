4402e74ffffdd37664927bf579ce919d8956ba6d
Nikolay Nesterovich <nesterovish@yandex.ru>
2020-01-31 15:46:44 +0300

    Изменеа страница контакт

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request, flash
 from config import Configuration
 from forms import ContactForm
+from flask_mail import Message, Mail
 import os
 
+mail = Mail()
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.secret_key = '1580208980-7d702e450c96ee352e41d401c014b0d07b9124de'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'nesterovish84@gmail.com'
+app.config["MAIL_PASSWORD"] = 'Lokomotiv1'
+ 
+mail.init_app(app)
 
 
 @app.route('/')
@@ -22,7 +32,18 @@ def contact():
   form = ContactForm()
  
   if request.method == 'POST':
-    return 'Form posted.'
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('contact.html', form=form)
+    else:
+      msg = Message(form.subject.data, sender='contact@example.com', recipients=['nesterovish84@gmail.com'])
+      msg.body = """
+      From: %s &lt;%s&gt;
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+ 
+      return render_template('contact.html', success=True)
  
   elif request.method == 'GET':
     return render_template('contact.html', form=form)
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 class Configuration(object):
     DEBUG = True
+    
